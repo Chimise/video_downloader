11fe863c9a817a9c8a5b4cc31fa399501d9af989
chisom promise <chimisepro@gmail.com>
2023-04-04 10:20:51 +0100

    feat: Created a dynamic and reusable controller for fetching video metadata

---
+++ src/controllers/media.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest,  NextApiResponse} from "next";
+import BaseExtractor from "@/extractors/base";
+import { getQuery, handleError } from "@/utils";
+import RequestError from "@/utils/request_error";
+
+export const getMediaMetaData = <T extends BaseExtractor>(Extractor: new (url: string) => T) => async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        switch (req.method) {
+            case 'GET':
+                const url = getQuery(req, 'url');
+                if (!url) {
+                    throw new RequestError('Url query missing', 400);
+                }
+                console.log(url);
+                const extractor = new Extractor(url);
+                const videoProps = await extractor.extractVideo();
+                return res.json(videoProps);
+            default:
+                throw new RequestError('Method not allowed', 405);
+
+        }
+
+    } catch (err) {
+        return handleError(err, res);
+    }
+}
